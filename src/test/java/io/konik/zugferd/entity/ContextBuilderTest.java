7642c4b7d3b994b2a8c882ce1b03452a3388bfee
Vad1mo <Vad1mo@users.noreply.github.com>
2014-09-24 00:29:02 +0200

    tryout with builder pattern

---
+++ src/test/java/io/konik/zugferd/entity/ContextBuilderTest.java
@@ -0,0 +1,23 @@
+package io.konik.zugferd.entity;
+
+import io.konik.zugferd.InvoiceBuilder;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ContextBuilderTest {
+
+   @Before
+   public void setUp() throws Exception {
+   }
+   
+   
+   @Test
+   public void test(){
+      ContextBuilder build = new ContextBuilder();
+//      InvoiceBuilder invoiceBuilder = new       InvoiceBuilder();
+//invoiceBuilder.
+
+   }
+
+}
